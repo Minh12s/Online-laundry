d7a97da3fb8df3252269f12a51bbf4210ea80193
dungdang <dungprohn1409@gmail.com>
2024-03-17 20:06:54 +0700

    update template

--- Controllers/PageController.cs
+++ Controllers/PageController.cs
@@ -1,8 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
+//using OgainShop.Models;
+//using OgainShop.Data;
+using Microsoft.AspNetCore.Http;
+//using OgainShop.Models.Authentication;
+using Microsoft.EntityFrameworkCore;
+using BCrypt.Net;
+using System;
+using System.Text;
+//using OgainShop.Heplers;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,7 +19,31 @@ namespace OnlineJwellery_Shopping.Controllers
     public class PageController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index()
+        public async Task<IActionResult> Home()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Details()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Category()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Checkout()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Contact()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Blog()
+        {
+            return View();
+        }
+        public async Task<IActionResult> About()
         {
             return View();
         }
