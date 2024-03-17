5f3759595d42ee8d5a9ecca3ab4b1344528502fa
Minh12s <minhtnth2209037@fpt.edu.vn>
2024-03-17 23:48:56 +0700

    update template admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineJwellery_Shopping.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View("DashboardAdmin/Dashboard");
+        }
+    }
+}
